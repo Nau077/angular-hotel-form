0db5ddfbce82bd35358f0b8f6432f8da96690711
roman.tolokontsev <roman.tolokontsev@nw.rt.ru>
2019-11-08 15:14:35 +0300

    fix form-service-ts

--- src/app/hotel-form/hotel-form.component.ts
+++ src/app/hotel-form/hotel-form.component.ts
@@ -1,150 +1,148 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl  } from '@angular/forms';
-import {Form, FormService, IserverData} from '../shared/form.service';
-import {ServerService} from '../shared/server-interact.service';
+import {Form, FormService} from '../shared/form.service';
 import * as moment from 'moment';
 
-// tslint:disable
 
- class HotelData {
-  low: { date_from: string; date_to: string; econom: number; standart: number; lux: number; child_discount_perc: number; };
-  high: { date_from: string; date_to: string; econom: number; standart: number; lux: number; child_discount_perc: number; };
-  low2: { date_from: string; date_to: string; econom: number; standart: number; lux: number; child_discount_perc: number; };
+class HotelData {
+low: { date_from: string; date_to: string; econom: number; standart: number; lux: number; child_discount_perc: number; };
+high: { date_from: string; date_to: string; econom: number; standart: number; lux: number; child_discount_perc: number; };
+low2: { date_from: string; date_to: string; econom: number; standart: number; lux: number; child_discount_perc: number; };
 
-      constructor() {
-          this.low = {
-            date_from: '15.05',
-            date_to: '20.06',
-            econom: 1000,
-            standart: 1500,
-            lux: 2000,
-            child_discount_perc: 50
-          };
-          this.high = {
-            date_from: '21.06',
-            date_to: '20.08',
-            econom: 1800,
-            standart: 2800,
-            lux: 4000,
-            child_discount_perc: 25
-          };
-          this.low2 = {
-              date_from: '21.08',
-              date_to: '15.09',
-              econom: 1200,
-              standart: 1800,
-              lux: 2300,
-              child_discount_perc: 25
-          }
-      }
-  }
+constructor() {
+	this.low = {
+	date_from: '15.05',
+	date_to: '20.06',
+	econom: 1000,
+	standart: 1500,
+	lux: 2000,
+	child_discount_perc: 50
+	};
+	this.high = {
+	date_from: '21.06',
+	date_to: '20.08',
+	econom: 1800,
+	standart: 2800,
+	lux: 4000,
+	child_discount_perc: 25
+	};
+	this.low2 = {
+		date_from: '21.08',
+		date_to: '15.09',
+		econom: 1200,
+		standart: 1800,
+		lux: 2300,
+		child_discount_perc: 25
+	}
+}
+}
 
 function getDataInsts () {
-  const dataInstance = new HotelData()
-  return dataInstance
+const dataInstance = new HotelData()
+return dataInstance
 }
 
 const selectedIntervals = ['econom', 'standart', 'lux']
 
 
 @Component({
-  selector: 'app-hotel-form',
-  templateUrl: './hotel-form.component.html',
-  styleUrls: ['./hotel-form.component.scss']
+selector: 'app-hotel-form',
+templateUrl: './hotel-form.component.html',
+styleUrls: ['./hotel-form.component.scss']
 })
 export class HotelFormComponent implements OnInit {
-  hotelCalcForm: FormGroup;
-  selectedIntervals = selectedIntervals;
-  titlePrice: string | number = '';
-  isCalculated: boolean = false;
-  adultCount = '';
-  middleChildCount = '';
-  littleChildCount = '';
-  isChildChecked = true;
-  period = '';
+hotelCalcForm: FormGroup;
+selectedIntervals = selectedIntervals;
+titlePrice: string | number = '';
+isCalculated: boolean = false;
+adultCount = '';
+middleChildCount = '';
+littleChildCount = '';
+isChildChecked = true;
+period = '';
 
-  constructor(private fb: FormBuilder, private formService: FormService) {
+constructor(private fb: FormBuilder, private formService: FormService) {
 
-  }
-
-ngOnInit() {
-  this.initForm();
 }
 
-  // Инициализация формы
- initForm() {
-  this.hotelCalcForm = this.fb.group({
-    date: [{begin: new Date(2019, 4, 15), end: new Date(2019, 8, 15)}
-    ],
-    adultsCount: [0, [
-      Validators.required,
-      Validators.pattern(/^[1-9]\d*$/)
-    ]],
-    childMiddleAgeCount:  [0, [
-      Validators.required,
-      Validators.pattern(/^[0-9]\d*$/)
-    ]],
-    childSmallAgeCount:  [0, [
-      Validators.required,
-      Validators.pattern(/^[0-9]\d*$/)
-    ]],
-    selectedPeriod: [selectedIntervals[0],
-    [
-      Validators.required
-    ]],
-  });
- }
+ngOnInit() {
+	this.initForm();
+}
 
- isControlInvalid(controlName: string): boolean {
-  const control = this.hotelCalcForm.controls[controlName];
-  const result = control.invalid && control.touched;
-  return result;
-  }
+// Инициализация формы
+initForm() {
+	this.hotelCalcForm = this.fb.group({
+	date: [{begin: new Date(2019, 4, 15), end: new Date(2019, 8, 15)}
+	],
+	adultsCount: [0, [
+	Validators.required,
+	Validators.pattern(/^[1-9]\d*$/)
+	]],
+	childMiddleAgeCount:  [0, [
+	Validators.required,
+	Validators.pattern(/^[0-9]\d*$/)
+	]],
+	childSmallAgeCount:  [0, [
+	Validators.required,
+	Validators.pattern(/^[0-9]\d*$/)
+	]],
+	selectedPeriod: [selectedIntervals[0],
+	[
+	Validators.required
+	]],
+	});
+}
 
-  calculate() {
+isControlInvalid(controlName: string): boolean {
+	const control = this.hotelCalcForm.controls[controlName];
+	const result = control.invalid && control.touched;
+	return result;
+}
+
+calculate() {
 	this.isChildChecked = true;
-    const controls = this.hotelCalcForm.controls;
-    // Проверяем форму на валидность
-    if (this.hotelCalcForm.invalid) {
-       // Если форма не валидна, то помечаем все контролы как touched
-      Object.keys(controls)
-       .forEach(controlName => controls[controlName].markAsTouched());
-       // Прерываем выполнение метода
-      return;
-      }
+	const controls = this.hotelCalcForm.controls;
+	// Проверяем форму на валидность
+	if (this.hotelCalcForm.invalid) {
+	// Если форма не валидна, то помечаем все контролы как touched
+	Object.keys(controls)
+	.forEach(controlName => controls[controlName].markAsTouched());
+	// Прерываем выполнение метода
+	return;
+}
 
-    const timeBegin = moment(this.hotelCalcForm.value.date.begin).format('MM-DD-YYYY');
-    const timeEnd = moment(this.hotelCalcForm.value.date.end).format('MM-DD-YYYY');
+const timeBegin = moment(this.hotelCalcForm.value.date.begin).format('MM-DD-YYYY');
+const timeEnd = moment(this.hotelCalcForm.value.date.end).format('MM-DD-YYYY');
 
-    const form: Form = {
-        period: this.hotelCalcForm.value.selectedPeriod,
-        adultsCount: this.hotelCalcForm.value.adultsCount,
-        childMiddleAgeCount: this.hotelCalcForm.value.childMiddleAgeCount,
-        childSmallAgeCount:  this.hotelCalcForm.value.childSmallAgeCount,
-        begin: timeBegin,
-        end: timeEnd
-      };
+const form: Form = {
+period: this.hotelCalcForm.value.selectedPeriod,
+adultsCount: this.hotelCalcForm.value.adultsCount,
+childMiddleAgeCount: this.hotelCalcForm.value.childMiddleAgeCount,
+childSmallAgeCount:  this.hotelCalcForm.value.childSmallAgeCount,
+begin: timeBegin,
+end: timeEnd
+};
 
-      // tslint:disable-next-line: align
-	const checkChild = this.formService.checkSmallAgeCount(form)
+// tslint:disable-next-line: align
+const checkChild = this.formService.checkSmallAgeCount(form)
 	if (!checkChild) {
-		this.isChildChecked = false
-		this.isCalculated = false
-		return
+	this.isChildChecked = false
+	this.isCalculated = false
+return
+}
+const price = this.formService.calculateForm(form, getDataInsts ());
+this.titlePrice = price
+
+this.adultCount = this.hotelCalcForm.value.adultsCount;
+this.middleChildCount = this.hotelCalcForm.value.childMiddleAgeCount;
+this.littleChildCount = this.hotelCalcForm.value.childSmallAgeCount;
+this.period = this.hotelCalcForm.value.selectedPeriod;
+
+if ((typeof this.titlePrice)=='number') {
+	this.isCalculated = true
+} else {
+this.isCalculated = false
+		}
 	}
-    const price = this.formService.calculateForm(form, getDataInsts ());
-    this.titlePrice = price
-
-    this.adultCount = this.hotelCalcForm.value.adultsCount;
-    this.middleChildCount = this.hotelCalcForm.value.childMiddleAgeCount;
-    this.littleChildCount = this.hotelCalcForm.value.childSmallAgeCount;
-    this.period = this.hotelCalcForm.value.selectedPeriod;
-
-    if ((typeof this.titlePrice)=='number') {
-      this.isCalculated = true
-    } else {
-      this.isCalculated = false
-    }
-  }
 }
 
--- src/app/shared/form.service.ts
+++ src/app/shared/form.service.ts
@@ -1,158 +1,148 @@
-import {Injectable} from '@angular/core';
-import * as moment from 'moment';
-import * as MomentRange from 'moment-range';
-import * as _ from 'underscore';
+	import {Injectable} from '@angular/core';
+	import * as moment from 'moment';
+	import * as MomentRange from 'moment-range';
+	import * as _ from 'underscore';
+	const Moment = MomentRange.extendMoment(moment);
 
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
-import { Message } from '@angular/compiler/src/i18n/i18n_ast';
-
-export interface IserverData {
-  low: {
-    date_from: string,
-    date_to: string,
-    econom: number,
-    standart: number,
-    lux?: number,
-    child_discount_perc: number
-  };
-  readonly high: {
-    date_from: string,
-    date_to: string,
-    econom: number,
-    standart: number,
-    lux: number,
-    child_discount_perc: number
-  };
-  low2: {
-    date_from: string,
-    date_to: string,
-    econom: number,
-    standart: number,
-    lux: number,
-    child_discount_perc: number
-  };
-}
-
-export interface Form {
-  adultsCount: number;
-  childMiddleAgeCount: number;
-  childSmallAgeCount: number;
-  begin: string;
-  end: string;
-  period: string;
-}
-
-@Injectable({providedIn: 'root'})
-export class FormService {
-
-// tslint:disable
-
-  // сложить в массив range все дни одного интервала
-  // сложить в массив rangeLow все дни в массивы интервалов data.low.date_from, data.low.date_to и т.д
-  // написать функцию, которая находит количество одинаковых элементов (дней) в этих двух массивах
-  // вычислить количество дней из каждого промежутка, которые оплатил пользователь
-  // посчитать деньги на каждый день из каждого промежутка
-  // сложить сумму
-
-
-  static getPrice(begin:string, end: string, data:object, form:object){
-    const Moment = MomentRange.extendMoment(moment);
-
-    const getBaseInterval = () => {
-      const dataArray = Object.entries(data)
-      const start = moment(new Date((dataArray[0] as any)[1].date_from))
-      const end = moment(new Date (dataArray[dataArray.length - 1][1].date_to))
-      const range = Moment.range(Moment(start), Moment(end));
-      const momentArray = Array.from(range.by('day'))
-      const days = momentArray.map(el => moment((el as any)._d).format('MM-DD-YYYY'))
-      return days
-    }
-
-    const getFormDaysInterval = ((begin, end) => {
-      const Start = moment((new Date(begin))), End = moment(new Date(end))
-      const range = Moment.range(moment(Start), moment(End));
-      const momentArray = Array.from(range.by('day'))
-      return momentArray.map(el => moment((el as any)._d).format('MM-DD-YYYY'))
-    })(begin, end);
-
-    const checkDifference = _.difference(getFormDaysInterval, getBaseInterval())
-
-    if (checkDifference.length) {
-      throw "Некорректный интервал между датами, выберите между 15 мая и 15 сентября";
-    }
-
-
-    const getDataDaysInterval = Object.entries(data).map((el) => {
-      const start = new Date(el[1].date_from)
-      const end = new Date(el[1].date_to)
-      const range = Moment.range(Moment(start), Moment(end));
-      const momentArray = Array.from(range.by('day'))
-      const days = momentArray.map(el => moment((el as any)._d).format('MM-DD-YYYY'))
-      el[1].daysInterval = days
-      return el;
-        });
-
-
-      const calculateDays = ((dataDays, formInterval, form ) => {
-        const  priceFactoryFn = ((dataDays) => {
-              return dataDays.reduce((acc, el) => {
-                let countSameDays  =_.intersection(formInterval, el[1].daysInterval)
-                if (formInterval.length > 1) countSameDays.pop();
-                const adultCount = (form as any).adultsCount
-                const childCount = (form as any).childMiddleAgeCount
-                const formPeriod = (form as any).period
-                const elPeriodPrice = el[1][formPeriod]
-                const discount_perc = el[1].child_discount_perc / 100
-                let price = countSameDays.length * elPeriodPrice * adultCount
-                let priceForCount = countSameDays.length * elPeriodPrice * childCount
-                if (countSameDays.length == 0) {
-                  price = 0
-                  priceForCount = 0
-                }
-                if (childCount>0) {
-                const childPerc = childCount * countSameDays.length * elPeriodPrice * discount_perc
-                const priceChild = priceForCount - childPerc
-                return acc + price + priceChild
-                }
-                return acc + price
-              }, 0)
-            })(dataDays)
-            return priceFactoryFn
-            })( getDataDaysInterval, getFormDaysInterval, form)
-        return calculateDays
- }
-
- static normalizeDataDate(data: object) {
-
-  return Object.entries(data).reduce((acc, [k, v]) => {
-      function normalize (date:string) {
-        const pointDate:string = date.toString().split('.').join("-")
-        const year:string = new Date().getFullYear().toString()
-        const dateNormalized = (pointDate:string) => pointDate.split('-').reverse().join("-")
-        const finalDate = dateNormalized(pointDate) + "-" + year
-        return finalDate
-      }
-
-      v.date_from = (moment(normalize(v.date_from), "MM-DD-YYYY") as any)._d.toString()
-      v.date_to = (moment(normalize(v.date_to), "MM-DD-YYYY") as any)._d.toString()
-      return { ...acc, [k]: v };
-        }, {});
-   }
-
-calculateForm(form: Form, dataServer:IserverData) {
-    const normalizeData = FormService.normalizeDataDate(dataServer);
-    try {
-      return FormService.getPrice(form.begin, form.end, normalizeData, form)
-    } catch (e) {
-     return e
-    }
-  }
-checkSmallAgeCount(form:Form) {
-		const child = form.childSmallAgeCount
-		const adult = form.adultsCount
-		if (child/adult > 3) return false
-		return true	
+	export interface IserverData {
+	low: {
+		date_from: string,
+		date_to: string,
+		econom: number,
+		standart: number,
+		lux?: number,
+		child_discount_perc: number
+	};
+	readonly high: {
+		date_from: string,
+		date_to: string,
+		econom: number,
+		standart: number,
+		lux: number,
+		child_discount_perc: number
+	};
+	low2: {
+		date_from: string,
+		date_to: string,
+		econom: number,
+		standart: number,
+		lux: number,
+		child_discount_perc: number
+		};
 	}
-}
+
+	export interface Form {
+		adultsCount: number;
+		childMiddleAgeCount: number;
+		childSmallAgeCount: number;
+		begin: string;
+		end: string;
+		period: string;
+	}
+
+	function getFormDays(begin:string, end: string) {
+		const startMoment = moment((new Date(begin))), 
+				endMoment = moment(new Date(end)),
+				range = Moment.range(moment(startMoment), moment(endMoment)),
+				momentArray = Array.from(range.by('day'))
+		return momentArray.map(el => moment((el as any)._d).format('MM-DD-YYYY'))
+	};
+
+	function getBaseDays(data:object) {
+		const dataArray = Object.entries(data),
+			start = moment(new Date((dataArray[0] as any)[1].date_from)),
+			end = moment(new Date (dataArray[dataArray.length - 1][1].date_to)),
+			range = Moment.range(Moment(start), Moment(end)),
+			momentArray = Array.from(range.by('day')),
+			days = momentArray.map(el => moment((el as any)._d).format('MM-DD-YYYY'))
+		return days
+	}
+
+	function getDataDays (data:object) {
+		return Object.entries(data).map((el) => {
+			const start = new Date(el[1].date_from),
+				end = new Date(el[1].date_to),
+				range = Moment.range(Moment(start), Moment(end)),
+				momentArray = Array.from(range.by('day')),
+				days = momentArray.map(el => moment((el as any)._d).format('MM-DD-YYYY'))
+			el[1].daysInterval = days
+			return el;
+		});
+	}
+	
+	function calculatePrice (dataDays, formInterval, form) {
+		const  priceFactoryFn = ((dataDays) => {
+			return dataDays.reduce((acc, el) => {
+			let countSameDays  =_.intersection(formInterval, el[1].daysInterval)
+			if (formInterval.length > 1) countSameDays.pop();
+			const adultCount = (form as any).adultsCount,
+				childCount = (form as any).childMiddleAgeCount,
+				formPeriod = (form as any).period,
+				elPeriodPrice = el[1][formPeriod],
+				discount_perc = el[1].child_discount_perc / 100
+			let price = countSameDays.length * elPeriodPrice * adultCount
+			let priceForCount = countSameDays.length * elPeriodPrice * childCount
+			if (countSameDays.length == 0) {
+				price = 0
+				priceForCount = 0
+			}
+			if (childCount>0) {
+			const childPerc = childCount * countSameDays.length * elPeriodPrice * discount_perc
+			const priceChild = priceForCount - childPerc
+			return acc + price + priceChild
+			}
+			return acc + price
+			}, 0)
+		})(dataDays)
+		return priceFactoryFn
+	}
+
+	@Injectable({providedIn: 'root'})
+	export class FormService {
+
+	static getPrice(begin:string, end: string, data:object, form:object){
+		const getBaseInterval:Array<string> = getBaseDays(data)
+		const getFormDaysInterval:Array<string> = getFormDays(begin, end);
+		const checkDifference:Array<string | []> = _.difference(getFormDaysInterval, getBaseInterval);
+
+	if (checkDifference.length) {
+		throw "Некорректный интервал между датами, выберите между 15 мая и 15 сентября";
+	}
+
+	const getDataDaysInterval:Array<any> = getDataDays(data);
+	const finalPrice = calculatePrice( getDataDaysInterval, getFormDaysInterval, form);
+		return finalPrice
+	}
+
+	static normalizeDataDate(data: object) {
+
+	return Object.entries(data).reduce((acc, [k, v]) => {
+		function normalize (date:string) {
+			const pointDate:string = date.toString().split('.').join("-"),
+				year:string = new Date().getFullYear().toString(),
+				dateNormalized = (pointDate:string) => pointDate.split('-').reverse().join("-"),
+				finalDate = dateNormalized(pointDate) + "-" + year
+		return finalDate
+		}
+
+	v.date_from = (moment(normalize(v.date_from), "MM-DD-YYYY") as any)._d.toString()
+	v.date_to = (moment(normalize(v.date_to), "MM-DD-YYYY") as any)._d.toString()
+		return { ...acc, [k]: v };
+		}, {});
+	}
+	
+	calculateForm(form: Form, dataServer:IserverData) {
+		const normalizeData = FormService.normalizeDataDate(dataServer);
+		try {
+			return FormService.getPrice(form.begin, form.end, normalizeData, form)
+		} catch (e) {
+			return e
+		}
+	}
+	checkSmallAgeCount(form:Form) {
+		const child = form.childSmallAgeCount,
+			adult = form.adultsCount
+		if (child/adult > 3) return false
+			return true	
+		}
+	}
